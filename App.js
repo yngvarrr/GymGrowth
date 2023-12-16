7f0c0cf833e036f6a54a2b35302d06558f1a79e9
Igor Fernandes <palusterra@gmail.com>
2023-12-16 18:48:38 -0300

    ajustando os serviços de navegação

--- App.js
+++ App.js
@@ -1,24 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet, Text, View } from 'react-native';
-import { HomeScreen } from './src/screens/HomeScreen';
-import { FolderScreen } from './src/screens/FolderScreen';
-import { MuscScreen } from './src/screens/Treinos/MuscScreen';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet, Text, View } from "react-native";
+import { MuscScreen } from "./src/screens/Treinos/MuscScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { TabNavigation } from "./src/navigation/TabNavigation";
 
-const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="GymGrowth" component={HomeScreen} />
-        <Tab.Screen name="Central de Treinos" component={FolderScreen} />
-      </Tab.Navigator>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Musculação" component={MuscScreen}/>
+        <Stack.Screen name="GymGrowth" component={TabNavigation} />
+        <Stack.Screen name="Musculação" component={MuscScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -27,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/navigation/TabNavigation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { HomeScreen } from "../screens/HomeScreen";
+import { FolderScreen } from "../screens/FolderScreen";
+
+const Tab = createBottomTabNavigator();
+
+export const TabNavigation = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="GymGrowth" component={HomeScreen} />
+      <Tab.Screen name="Central de Treinos" component={FolderScreen} />
+    </Tab.Navigator>
+  );
+};
