50c9064d064dcb6ed9fe3b9c0982bb412bfdee01
Igor Fernandes <palusterra@gmail.com>
2024-01-13 21:44:55 -0300

    mudando o design da tela de adição de exercício

--- src/screens/Treinos/Musculação/AddWorkout.jsx
+++ src/screens/Treinos/Musculação/AddWorkout.jsx
@@ -10,13 +10,58 @@ import {
 } from "../../../components/firebase/index";
 import Button from "../../../components/Button";
 import { AntDesign } from "@expo/vector-icons";
-import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message';
+import Toast from "react-native-toast-message";
+import Checkbox from "expo-checkbox";
+import { SelectList } from "react-native-dropdown-select-list";
+import DropDownPicker from "react-native-dropdown-picker";
 
 export const AddWorkout = () => {
+  const [selected, setSelected] = React.useState([]);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
   const [treino, setTreino] = useState("");
   const [carga, setCarga] = useState("");
   const [grupo, setGrupo] = useState("");
   const [tipo, setTipo] = useState("");
+  const [falha, setFalha] = useState(false);
+  const [inferiores, setInferiores] = useState(false);
+  const [superiores, setSuperiores] = useState(false);
+  const [biceps, setBiceps] = useState(false);
+  const [antebraco, setAntebraco] = useState(false);
+  const [peito, setPeito] = useState(false);
+  const [triceps, setTriceps] = useState(false);
+  const [ombros, setOmbros] = useState(false);
+  const [costas, setCostas] = useState(false);
+  const [quad, setQuad] = useState(false);
+  const [posterior, setPoserior] = useState(false);
+  const [pant, setPant] = useState(false);
+  const [gluteo, setGluteeo] = useState(false);
+
+  const [grupos, setGrupos] = useState([
+    { label: "Bíceps", value: "Bíceps" },
+    { label: "Tríceps", value: "Tríceps" },
+    { label: "Peito", value: "Peito" },
+    { label: "Ombros", value: "Ombros" },
+    { label: "Costas", value: "Costas" },
+    { label: "Antebraço", value: "Antebraço" },
+    { label: "Quadríceps", value: "Quadríceps" },
+    { label: "Panturrilha", value: "Panturrilha" },
+    { label: "Posterior", value: "Posterior" },
+    { label: "Glúteo", value: "Glúteo" },
+  ]);
+
+  const data = [
+    { key: "1", value: "Bíceps" },
+    { key: "2", value: "Tríceps" },
+    { key: "3", value: "Peito" },
+    { key: "4", value: "Ombros" },
+    { key: "5", value: "Costas" },
+    { key: "6", value: "Antebraço" },
+    { key: "7", value: "Quadríceps" },
+    { key: "8", value: "Panturrilha" },
+    { key: "9", value: "Posterior" },
+    { key: "10", value: "Glúteo" },
+  ];
 
   const showToast = () => {
     Toast.show({
@@ -25,7 +70,7 @@ export const AddWorkout = () => {
       text2: "Seu exercício foi adicionado! 👋",
       autoHide: true,
       visibilityTime: 2500,
-      position: 'bottom',
+      position: "bottom",
     });
   };
 
@@ -36,12 +81,14 @@ export const AddWorkout = () => {
         tipo: tipo,
         grupo: grupo,
         carga: carga,
+        falha: falha,
       });
       console.log("Document written with ID: ", docRef.id);
       setTreino("");
       setCarga("");
       setGrupo("");
       setTipo("");
+      setFalha();
     } catch (e) {
       console.error("Error adding document: ", e);
     }
@@ -50,7 +97,7 @@ export const AddWorkout = () => {
   const handleSubmit = () => {
     addTreino();
     showToast();
-  }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -70,38 +117,66 @@ export const AddWorkout = () => {
           />
         </View>
         <View style={styles.inputContainer}>
-          <Text style={styles.inputHead}>Grupo muscular:</Text>
-          <TextInput
-            placeholder="Qual músculo trabalha?"
-            style={styles.input}
-            value={grupo}
-            onChangeText={(text) => setGrupo(text)}
-            onSubmitEditing={addTreino}
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <Text style={styles.inputHead}>Membros:</Text>
-          <TextInput
-            placeholder="Superiores ou inferiores?"
-            style={styles.input}
-            value={tipo}
-            onChangeText={(text) => setTipo(text)}
-            onSubmitEditing={addTreino}
-          />
-        </View>
-        <View style={styles.inputContainer}>
           <Text style={styles.inputHead}>Carga atual (kg):</Text>
           <TextInput
             placeholder="Quantos kg pega?"
             style={styles.input}
             value={carga}
-            onChangeText={(text) => setCarga(text)}
+            onChangeText={(text) => setCarga(text + " kg")}
             onSubmitEditing={addTreino}
             keyboardType="numeric"
           />
         </View>
-        <Button label="Adicionar exercício" onPress={handleSubmit} />
-        <Toast/>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputHead}>Grupo muscular:</Text>
+          <SelectList
+            setSelected={(val) => setSelected(val)}
+            data={data}
+            save="value"
+          />
+          {/*<TextInput
+            placeholder="Qual músculo trabalha?"
+            style={styles.input}
+            value={grupo}
+            onChangeText={(text) => setGrupo(text)}
+            onSubmitEditing={addTreino}
+  /> */}
+        </View>
+        <View style={styles.membrosContainer}>
+          <Text style={styles.inputHead}>Membros:</Text>
+          <View style={styles.checksContainer}>
+            <Checkbox value={inferiores} onValueChange={setInferiores} />
+            <Text style={styles.membrosText}>Inferiores</Text>
+          </View>
+          <View style={styles.checksContainer}>
+            <Checkbox value={superiores} onValueChange={setSuperiores} />
+            <Text style={styles.membrosText}>Superiores</Text>
+          </View>
+
+          {/*<TextInput
+            placeholder="Superiores ou inferiores?"
+            style={styles.input}
+            value={tipo}
+            onChangeText={(text) => setTipo(text)}
+            onSubmitEditing={addTreino}
+/> */}
+        </View>
+        <View style={styles.confirmContainer}>
+          <View style={styles.checkboxContainer}>
+            <Checkbox value={falha} onValueChange={setFalha} />
+            <Text style={styles.checkText}>Até a falha?</Text>
+          </View>
+          <Button label="Adicionar exercício" onPress={handleSubmit} />
+          <DropDownPicker
+            open={open}
+            value={value}
+            items={grupos}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setGrupos}
+          />
+        </View>
+        <Toast />
       </View>
     </SafeAreaView>
   );
@@ -115,37 +190,54 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     borderWidth: 2,
     borderRadius: 15,
-    width: 170,
+    width: 200,
   },
   head: {
-    fontSize: 22,
+    fontSize: 24,
     fontWeight: "bold",
   },
   container: {
     marginTop: 40,
     flex: 1,
     width: "100%",
-    height: '100%'
+    height: "100%",
   },
   inputContainer: {
     alignItems: "center",
-    maxWidth: 150,
   },
   inputsGrid: {
-    flexDirection: "row",
-    flexWrap: "wrap",
     alignItems: "center",
     justifyContent: "space-around",
     margin: 20,
   },
   inputHead: {
     textAlign: "center",
-    fontSize: 16,
-    fontWeight: "400",
+    fontSize: 20,
+    fontWeight: "500",
   },
   header: {
     flexDirection: "row",
     alignItems: "center",
     padding: 5,
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+  },
+  checkText: {
+    fontSize: 20,
+    fontWeight: "500",
+    marginLeft: 5,
+  },
+  checksContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 5,
+  },
+  membrosText: {
+    marginLeft: 5,
+    fontWeight: "500",
+    fontSize: 16,
   },
 });
